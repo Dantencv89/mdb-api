45df59063b7121c3008196a6abae099121f1625f
Dantencv89 <nachocv1989@gmail.com>
2021-11-23 20:49:50 +0100

    mongo localhost added

--- src/database/database.js
+++ src/database/database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 (async()=>{
     try{
-        const db = await mongoose.connect(process.env.MONGODB_URI);
+        const db = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/taskapi');
     }
     catch (error){
         console.log(`Error at opening MongoDB Database: ${error}`);
